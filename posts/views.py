1507a44d503eff3c6bc045d524382b18c5a02fb5
yoshi-non <himekawa.yoshino44@gmail.com>
2022-10-21 22:34:25 +0900

    add: ログイン時の操作権限付与

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -6,11 +6,13 @@ from .serializers import PostSerializer
 
 # ListCreateAPIView -> read-write endpoint
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 # RetrieveUpdateDestoryAPIView -> ALlows read, update, delete
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
